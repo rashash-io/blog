c6af0fd872e0ff4d7236a51a97a319927b6e0e24
rashash-io <amr.rashad1@proton.me>
2026-03-01 01:43:46 +0200

    started seeing blog posts

--- app/blogpost/[slug]/page.tsx
+++ app/blogpost/[slug]/page.tsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
-const Blogpost = async ({ params }: { params: Promise<{ slug: string }> }) => {
+
+
+import { unified } from "unified"
+import remarkParse from "remark-parse"
+import remarkFrontmatter from "remark-frontmatter"
+import remarkParseFrontmatter from "remark-parse-frontmatter"
+import remarkRehype from "remark-rehype"
+import rehypeRaw from "rehype-raw"
+import rehypeStringify from "rehype-stringify"
+import rehypeHighlight from "rehype-highlight"
+import matter from 'gray-matter';
+import fs from 'fs';
+
+export const BlogPost = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const { slug } = await params;
+
+    const processor = unified()
+        .use(remarkParse)
+        .use(remarkRehype)
+        .use(rehypeStringify)
+
+
+    const filePath = `content/${slug}.md`
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const { data, content } = matter(fileContent);
+    const htmlContent = (await processor.process(content)).toString();
     return (
-        <div>Blogpost {slug}</div>
+        <>
+            <MaxWidthWrapper className="prose dark:prose-invert">
+                <div className="text-xl font-bold">{data.title}</div>
+                <div dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
+            </MaxWidthWrapper>
+            <div>Blogpost {slug}</div>
+        </>
     )
 }
 
-export default Blogpost
+
+
+export default BlogPost
 
 
